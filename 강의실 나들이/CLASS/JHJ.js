7b549243587aef7edf348b74e56875c6a438b4da
Hyungjin Joo <hjjooace@naver.com>
2019-11-27 17:55:19 +0900

    문 제작

--- 강의실 나들이/CLASS/JHJ.js
+++ 강의실 나들이/CLASS/JHJ.js
@@ -23,7 +23,7 @@ function makeCeiling(x, y, z, pos_x, pos_y, pos_z) {
     cube.position.z = pos_z;
     ClassRoom.add(cube);
 }
-function makeWall(x, y, z, pos_x, pos_y, pos_z) {
+function makeWall1(x, y, z, pos_x, pos_y, pos_z) {
     var cubeGeometry = new THREE.BoxGeometry(x, y, z);
     var texture = new THREE.TextureLoader().load('wall2.jpg');
     var cubeMaterial = new THREE.MeshBasicMaterial({ map: texture });
@@ -32,6 +32,17 @@ function makeWall(x, y, z, pos_x, pos_y, pos_z) {
     cube.position.x = pos_x;
     cube.position.y = pos_y;
     cube.position.z = pos_z;
+    ClassRoom.add(cube);
+}
+function makeWall2(x, y, z, pos_x, pos_y, pos_z) {
+    var cubeGeometry = new THREE.BoxGeometry(x, y, z);
+    var texture = new THREE.TextureLoader().load('wall2.jpg');
+    var cubeMaterial = new THREE.MeshBasicMaterial({ map: texture });
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.rotation.z = Math.PI*-0.5;
+    cube.position.x = pos_x-25;
+    cube.position.y = pos_y;
+    cube.position.z = pos_z+25;
     ClassRoom.add(cube);
 }
 
@@ -55,10 +66,19 @@ function drawClassRoom(scene) {
     }
     for (var i = 0; i < 16; i++) {
         for (var j = 0; j < 4; j++) {
-            makeWall(50.6, 0.6, 50.6, 2.5 + 50 * i, 10+ 50 * j,-25);
-            makeWall(50.6, 0.6, 50.6, 2.5 + 50 * i, 10+ 50 * j,425);
+            makeWall1(50.6, 0.6, 50.6, 2.5 + 50 * i, 10 + 50 * j, -25);
+            makeWall1(50.6, 0.6, 50.6, 2.5 + 50 * i, 10 + 50 * j, 425);
         }
     }
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 4; j++) {
+
+            makeWall2(50.6, 0.6, 50.6, 2.5, 10 + 50 * j, -25 + 50 * i);
+            if (!(i == 4 && j == 1)&&!(i == 4 && j == 0)&&!(i == 5 && j == 1)&&!(i == 5 && j == 0))
+                makeWall2(50.6, 0.6, 50.6, 802.5, 10 + 50 * j, -25 + 50 * i);
+        }
+    }
+
 
     scene.add(ClassRoom);
 }
