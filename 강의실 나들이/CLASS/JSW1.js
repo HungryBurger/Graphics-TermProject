1b42cc9820ade4073be43a93aae16cd4360ca6eb
wookdoop2 <44601663+wookdoop2@users.noreply.github.com>
2019-11-28 14:15:53 +0900

    1st update jsw

--- 강의실 나들이/CLASS/JSW1.js
+++ 강의실 나들이/CLASS/JSW1.js
@@ -1,32 +1,6 @@
-var group = new THREE.Group();
+var projector = new THREE.Group();
 
-function createProjectorBody0(scene) {
-
-	/**var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
-	camera.position.set( 5, 5, 30 );
-	var renderer = new THREE.WebGLRenderer({
-	  antialias: true
-	});
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );**/
-
-	//var controls = new THREE.OrbitControls( camera, renderer.domElement );
-
-	//var dirLight = new THREE.DirectionalLight( 0xffffff );
-	//dirLight.position.set( -10, 10, 10 );
-	//scene.add( dirLight );
-
-	/**var loader = new THREE.CubeTextureLoader();
-	loader.setCrossOrigin( "" );
-	loader.setPath( 'https://threejs.org/examples/textures/cube/pisa/' );**/
-
-	/**var cubeTexture = loader.load( [
-	  'px.png', 'nx.png',
-	  'py.png', 'ny.png',
-	  'pz.png', 'nz.png'
-	] );**/
-
-	//scene.background = cubeTexture;
+function createProjectorBody0(scene, a, b, c) {
 
 	function createBoxWithRoundedEdges0( width, height, depth, radius0, smoothness ) {
 	  let shape = new THREE.Shape();
@@ -60,14 +34,13 @@ function createProjectorBody0(scene) {
 
 	 let cube = new THREE.Mesh( createBoxWithRoundedEdges0( 2.5, 1, 2, 0.5 / 9, 16 ), cubeMat );
 	 cube.scale.setScalar( 1.25 );
-	 x_ = cube.position.x = 0;
-	 y_ = cube.position.y = 10;
-	 z_ = cube.position.z = 0;
+	 x_ = cube.position.x = a;
+	 y_ = cube.position.y = b;
+	 z_ = cube.position.z = c;
 	 scene.add( cube );
 	
 	 createProjectorBody1(scene, x_, y_, z_);
 	 createProjectorCamera(scene, x_, y_, z_);
-
 	 createProjectorLeftHanger0(scene, x_ - 0.5, y_, z_);
 	 createProjectorLeftHangerJoint0(scene, x_ - 0.5, y_, z_);
 	 createProjectorLeftHanger1(scene, x_ - 0.5, y_, z_);
@@ -87,20 +60,7 @@ function createProjectorBody0(scene) {
 	 createCable(scene, x_, y_, z_, 0.02, 0x2457B);
 	 createCable(scene, x_, y_, z_ - 0.2, 0.03, 0xED0000);
 	 
-	 createFluorescentBase0(scene, x_, y_, z_);
-	 createFluorescentBase1(scene, x_, y_, z_);
-	 createFluorescentBase2(scene, x_, y_, z_);
-	 createFluorescentBase3(scene, x_, y_, z_);
-	 //createFluorescentBase4(scene, x_, y_, z_);
-	 createFluorescentBase5(scene, x_, y_, z_);
-	 createFluorescentBase6(scene, x_, y_, z_);
-	 createFluorescentLamp0(scene, x_, y_, z_);
-	 createFluorescentLamp1(scene, x_, y_, z_);
-	 createFluorescentLampBase(scene, x_, y_, z_);
-	 
-	 group.scale.set(2, 2, 2);
-	 group.position.y = -15;
-	 scene.add(group);
+	 scene.add(projector);
 
 }
 
@@ -256,49 +216,78 @@ function createCable(scene, _x_, _y_, _z_, r, c) {
 
 //-----------------------------------------------------------------------------------
 
-function createFluorescentBase0(scene, _x_, _y_, _z_) {
+function createFluorescentBase0(scene, a, b, c, intensity) {
+
+    var spotLight = new THREE.SpotLight(0xFFFFFF, intensity, 0, (Math.PI / 180 * 80));
+    spotLight.position.set(a, b, c);
+    spotLight.castShadow = true;
+	spotLight.target.position.set(a, 0, c);
+	spotLight.shadow.mapSize.width = 512;
+    spotLight.shadow.mapSize.height = 512;
+    scene.add(spotLight.target);
+	scene.add(spotLight);
+
+	var fluorescent = new THREE.Group();
 	var geometry = new THREE.BoxGeometry( 0.1, 7, 2 );
 	var material = new THREE.MeshBasicMaterial( { color: 0xBDBDBD } );
+	material.castShadow= true;
 	var cube = new THREE.Mesh( geometry, material );
-	cube.position.x = _x_;
-	cube.position.y = _y_ + 6;
-	cube.position.z = _z_;
+
+	x_ = cube.position.x = a;
+	y_ = cube.position.y = b;
+	z_ = cube.position.z = c;
+
 	cube.rotateZ(Math.PI/360 * 180);
-		group.add(cube);
-	//scene.add( cube );
+	fluorescent.add(cube);
+
+	createFluorescentBase1(fluorescent, x_, y_, z_);
+	createFluorescentBase2(fluorescent, x_, y_, z_);
+	createFluorescentBase3(fluorescent, x_, y_, z_);
+	//createFluorescentBase4(scene, x_, y_, z_);
+	createFluorescentBase5(fluorescent, x_, y_, z_);
+	createFluorescentBase6(fluorescent, x_, y_, z_);
+	createFluorescentLamp0(fluorescent, x_, y_, z_);
+	createFluorescentLamp1(fluorescent, x_, y_, z_);
+	createFluorescentLampBase0(fluorescent, x_, y_, z_);
+	createFluorescentLampBase1(fluorescent, x_, y_, z_);
+	createFluorescentLampBase2(fluorescent, x_, y_, z_);
+	createFluorescentLampBase3(fluorescent, x_, y_, z_);
+	
+	fluorescent.position.y = -8;
+	scene.add(fluorescent);
 }
 
-function createFluorescentBase1(scene, _x_, _y_, _z_) {
+function createFluorescentBase1(fluorescent, _x_, _y_, _z_) {
 	var geometry = new THREE.BoxGeometry( 0.25, 7, 0.2 );
 	var material = new THREE.MeshBasicMaterial( { color: 0x5D5D5D } );
 	var cube = new THREE.Mesh( geometry, material );
 	cube.position.x = _x_;
-	cube.position.y = _y_ + 5.8;
+	cube.position.y = _y_ - 0.18;
 	cube.position.z = _z_ - 0.9;
 	cube.rotateZ(Math.PI/360 * 180);
-		group.add(cube);
+	fluorescent.add(cube);
 	//scene.add( cube );
 }
-function createFluorescentBase2(scene, _x_, _y_, _z_) {
+function createFluorescentBase2(fluorescent, _x_, _y_, _z_) {
 	var geometry = new THREE.BoxGeometry( 0.25, 7, 0.2 );
 	var material = new THREE.MeshBasicMaterial( { color: 0x5D5D5D } );
 	var cube = new THREE.Mesh( geometry, material );
 	cube.position.x = _x_;
-	cube.position.y = _y_ + 5.8;
+	cube.position.y = _y_ - 0.18;
 	cube.position.z = _z_ + 0.9;
 	cube.rotateZ(Math.PI/360 * 180);
-		group.add(cube);
+	fluorescent.add(cube);
 	//scene.add( cube );
 }
-function createFluorescentBase3(scene, _x_, _y_, _z_) {
+function createFluorescentBase3(fluorescent, _x_, _y_, _z_) {
 	var geometry = new THREE.CylinderGeometry( 0.28, 0.28, 7, 4, 1, false, 3.15, 3.15 );
 	var material = new THREE.MeshStandardMaterial( { color: 0xFCFCFC,  metalness: 0.1, roughness: 0} );
 	var cylinder = new THREE.Mesh( geometry, material );
 	cylinder.position.x = _x_;
-	cylinder.position.y = _y_ + 5.96;
+	cylinder.position.y = _y_ - 0.02;
 	cylinder.position.z = _z_;
 	cylinder.rotateZ(Math.PI/360 * 180);
-		group.add(cylinder);
+	fluorescent.add(cylinder);
 	//scene.add( cylinder );
 }
 /**function createFluorescentBase4(scene, _x_, _y_, _z_) {
@@ -311,57 +300,90 @@ function createFluorescentBase3(scene, _x_, _y_, _z_) {
 	cylinder.rotateZ(Math.PI/360 * 180);
 	scene.add( cylinder );
 }**/
-function createFluorescentBase5(scene, _x_, _y_, _z_) {
+function createFluorescentBase5(fluorescent, _x_, _y_, _z_) {
 	var geometry = new THREE.BoxGeometry( 0.2, 0.27, 2 );
 	var material = new THREE.MeshBasicMaterial( { color: 0x5D5D5D } );
 	var cube = new THREE.Mesh( geometry, material );
 	cube.position.x = _x_ + 3.4;
-	cube.position.y = _y_ + 5.81;
+	cube.position.y = _y_ - 0.17;
 	cube.position.z = _z_;
-		group.add(cube);
+	fluorescent.add(cube);
 	//scene.add( cube );
 }
-function createFluorescentBase6(scene, _x_, _y_, _z_) {
+function createFluorescentBase6(fluorescent, _x_, _y_, _z_) {
 	var geometry = new THREE.BoxGeometry( 0.2, 0.27, 2 );
 	var material = new THREE.MeshBasicMaterial( { color: 0x5D5D5D } );
 	var cube = new THREE.Mesh( geometry, material );
 	cube.position.x = _x_ - 3.4;
-	cube.position.y = _y_ + 5.81;
+	cube.position.y = _y_ - 0.17;
 	cube.position.z = _z_;
-		group.add(cube);
+	fluorescent.add(cube);
 	//scene.add( cube );
 }
-function createFluorescentLamp0(scene, _x_, _y_, _z_) {
+function createFluorescentLamp0(fluorescent, _x_, _y_, _z_) {
 	var geometry = new THREE.CylinderGeometry( 0.1, 0.1, 6.7, 32, 5 );
 	var cylinderMaterial = new THREE.MeshBasicMaterial( {color: 0xFFFFFF} );
 	var cylinder = new THREE.Mesh( geometry, cylinderMaterial, 2, 5 );
 	cylinder.position.x = _x_;
-	cylinder.position.y = _y_ + 5.8;
+	cylinder.position.y = _y_ - 0.18;
 	cylinder.position.z = _z_ + 0.55;
 	cylinder.rotateZ(Math.PI/360 * 180);
-		group.add(cylinder);
+	fluorescent.add(cylinder);
 	//scene.add( cylinder );
 }
-function createFluorescentLamp1(scene, _x_, _y_, _z_) {
+function createFluorescentLamp1(fluorescent, _x_, _y_, _z_) {
 	var geometry = new THREE.CylinderGeometry( 0.1, 0.1, 6.7, 32, 5 );
 	var cylinderMaterial = new THREE.MeshBasicMaterial( {color: 0xFFFFFF} );
 	var cylinder = new THREE.Mesh( geometry, cylinderMaterial, 2, 5 );
 	cylinder.position.x = _x_;
-	cylinder.position.y = _y_ + 5.8;
+	cylinder.position.y = _y_ - 0.18;
 	cylinder.position.z = _z_ - 0.55;
 	cylinder.rotateZ(Math.PI/360 * 180);
-		group.add(cylinder);
+	fluorescent.add(cylinder);
 	//scene.add( cylinder );
 }
-function createFluorescentLampBase(scene, _x_, _y_, _z_) {
-	var geometry = new THREE.CylinderGeometry( 0.15, 0.15, 1, 32, 5 );
+function createFluorescentLampBase0(fluorescent, _x_, _y_, _z_) {
+	var geometry = new THREE.CylinderGeometry( 0.13, 0.13, 0.15, 32, 5 );
 	var cylinderMaterial = new THREE.MeshBasicMaterial( {color: 0x000000} );
 	var cylinder = new THREE.Mesh( geometry, cylinderMaterial, 2, 5 );
-	cylinder.position.x = _x_;
-	cylinder.position.y = _y_ + 5.8;
+	cylinder.position.x = _x_ + 3.23;
+	cylinder.position.y = _y_ - 0.18;
+	cylinder.position.z = _z_ + 0.55;
+	cylinder.rotateZ(Math.PI/360 * 180);
+	fluorescent.add(cylinder);
+	//scene.add( cylinder );
+}
+function createFluorescentLampBase1(fluorescent, _x_, _y_, _z_) {
+	var geometry = new THREE.CylinderGeometry( 0.13, 0.13, 0.15, 32, 5 );
+	var cylinderMaterial = new THREE.MeshBasicMaterial( {color: 0x000000} );
+	var cylinder = new THREE.Mesh( geometry, cylinderMaterial, 2, 5 );
+	cylinder.position.x = _x_ - 3.23;
+	cylinder.position.y = _y_ - 0.18;
+	cylinder.position.z = _z_ + 0.55;
+	cylinder.rotateZ(Math.PI/360 * 180);
+	fluorescent.add(cylinder);
+	//scene.add( cylinder );
+}
+function createFluorescentLampBase2(fluorescent, _x_, _y_, _z_) {
+	var geometry = new THREE.CylinderGeometry( 0.13, 0.13, 0.15, 32, 5 );
+	var cylinderMaterial = new THREE.MeshBasicMaterial( {color: 0x000000} );
+	var cylinder = new THREE.Mesh( geometry, cylinderMaterial, 2, 5 );
+	cylinder.position.x = _x_ + 3.23;
+	cylinder.position.y = _y_ - 0.18;
 	cylinder.position.z = _z_ - 0.55;
 	cylinder.rotateZ(Math.PI/360 * 180);
-		group.add(cylinder);
+	fluorescent.add(cylinder);
+	//scene.add( cylinder );
+}
+function createFluorescentLampBase3(fluorescent, _x_, _y_, _z_) {
+	var geometry = new THREE.CylinderGeometry( 0.13, 0.13, 0.15, 32, 5 );
+	var cylinderMaterial = new THREE.MeshBasicMaterial( {color: 0x000000} );
+	var cylinder = new THREE.Mesh( geometry, cylinderMaterial, 2, 5 );
+	cylinder.position.x = _x_ - 3.23;
+	cylinder.position.y = _y_ - 0.18;
+	cylinder.position.z = _z_ - 0.55;
+	cylinder.rotateZ(Math.PI/360 * 180);
+	fluorescent.add(cylinder);
 	//scene.add( cylinder );
 }
 
