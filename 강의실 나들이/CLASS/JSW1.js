33cf5799207fbfc02f673ae3d1fb2f844f06e39c
sungwook <sunguk3391@gmail.com>
2019-12-02 22:49:27 +0900

    change fluorescent base0 color

--- 강의실 나들이/CLASS/JSW1.js
+++ 강의실 나들이/CLASS/JSW1.js
@@ -277,7 +277,7 @@ function createFluorescentBase0(B208D, a, b, c, intensity, p_x, p_y, p_z, scale)
 	var fluorescent = new THREE.Group();
 
 	// create fluorescent light
-    var spotLight = new THREE.SpotLight(0xFFFFFF, intensity, 0, (Math.PI / 180 * 120));
+    var spotLight = new THREE.SpotLight(0xEDEDED, intensity, 0, (Math.PI / 180 * 120));
     spotLight.position.set(a + p_x, b + p_y, c + p_z);
     spotLight.castShadow = true;
 	spotLight.target.position.set(a + p_x, 0, c + p_z);
@@ -349,7 +349,7 @@ function createFluorescentBase2(fluorescent, _x_, _y_, _z_) {
 // create fluorescent base
 function createFluorescentBase3(fluorescent, _x_, _y_, _z_) {
 	var geometry = new THREE.CylinderGeometry( 0.28, 0.28, 7, 4, 1, false, 3.15, 3.15 );
-	var material = new THREE.MeshBasicMaterial( { color: 0x5D5D5D,  metalness: 0.1, roughness: 0} );
+	var material = new THREE.MeshBasicMaterial( { color: 0xC9C9C9,  metalness: 0.1, roughness: 0} );
 	var cylinder = new THREE.Mesh( geometry, material );
 
 	// object rotation and set position
