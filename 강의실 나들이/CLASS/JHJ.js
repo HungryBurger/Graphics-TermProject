bb22c06cb7d87ec41589b8e1e6d490049e535071
Hyungjin Joo <hjjooace@naver.com>
2019-12-03 14:18:21 +0900

    Add Comment Main.js & JHJ.js

--- 강의실 나들이/CLASS/JHJ.js
+++ 강의실 나들이/CLASS/JHJ.js
@@ -1,6 +1,16 @@
+/*
+This file is for making Space & Screen
+Space is consist of two parts
+One is ClassRoom and the other is Aisle
+*/
+
 var ClassRoom = new THREE.Group();
 var Aisle = new THREE.Group();
 var Screen = new THREE.Group();
+//------------------------
+//This is ClassRoom part
+//-------------------------
+//This function for making ground of ClassRoom
 function makeGround(x, y, z, pos_x, pos_y, pos_z) {
     var cubeGeometry = new THREE.BoxGeometry(x, y, z);
     var texture = new THREE.TextureLoader().load('ClassRoom_Ground.jpg');
@@ -16,6 +26,7 @@ function makeGround(x, y, z, pos_x, pos_y, pos_z) {
     cube.position.z = pos_z;
     ClassRoom.add(cube);
 }
+//This function for making ceiling of ClassRoom
 function makeCeiling(x, y, z, pos_x, pos_y, pos_z) {
     var cubeGeometry = new THREE.BoxGeometry(x, y, z);
     var texture = new THREE.TextureLoader().load('ClassRoomGround.jpg');
@@ -32,12 +43,13 @@ function makeCeiling(x, y, z, pos_x, pos_y, pos_z) {
     cube.position.z = pos_z;
     ClassRoom.add(cube);
 }
+//This function for making wall of ClassRoom
 function makeWall1(x, y, z, pos_x, pos_y, pos_z) {
     var cubeGeometry = new THREE.BoxGeometry(x, y, z);
     var texture = new THREE.TextureLoader().load('ClassRoom_Wall.jpg');
     var cubeMaterial = new THREE.MeshPhongMaterial({ map: texture });
     var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-        
+
     //add shadow render
     cube.castShadow = true;
     cube.receiveShadow = true;
@@ -48,12 +60,13 @@ function makeWall1(x, y, z, pos_x, pos_y, pos_z) {
     cube.position.z = pos_z;
     ClassRoom.add(cube);
 }
+//This function for making wall of ClassRoom
 function makeWall2(x, y, z, pos_x, pos_y, pos_z) {
     var cubeGeometry = new THREE.BoxGeometry(x, y, z);
     var texture = new THREE.TextureLoader().load('ClassRoom_Wall.jpg');
     var cubeMaterial = new THREE.MeshPhongMaterial({ map: texture });
     var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-        
+
     //add shadow render
     cube.castShadow = true;
     cube.receiveShadow = true;
@@ -64,13 +77,13 @@ function makeWall2(x, y, z, pos_x, pos_y, pos_z) {
     cube.position.z = pos_z + 25;
     ClassRoom.add(cube);
 }
-//턱 벽면
+//This function for making wall of ClassRoom Entrance
 function makeWall3(x, y, z, pos_x, pos_y, pos_z) {
     var cubeGeometry = new THREE.BoxGeometry(x, y, z);
     var texture = new THREE.TextureLoader().load('Ground.jpg');
     var cubeMaterial = new THREE.MeshPhongMaterial({ map: texture });
     var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-        
+
     //add shadow render
     cube.castShadow = true;
     cube.receiveShadow = true;
@@ -82,13 +95,13 @@ function makeWall3(x, y, z, pos_x, pos_y, pos_z) {
     console.log(cube.position);
     ClassRoom.add(cube);
 }
-//턱 벽면
+//This function for making wall of ClassRoom Entrance 
 function makeWall4(x, y, z, pos_x, pos_y, pos_z) {
     var cubeGeometry = new THREE.BoxGeometry(x, y, z);
     var texture = new THREE.TextureLoader().load('Ground.jpg');
     var cubeMaterial = new THREE.MeshPhongMaterial({ map: texture });
     var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-        
+
     //add shadow render
     cube.castShadow = true;
     cube.receiveShadow = true;
@@ -100,15 +113,12 @@ function makeWall4(x, y, z, pos_x, pos_y, pos_z) {
     console.log(cube.position);
     ClassRoom.add(cube);
 }
-//천장
+//This function for making ceiling of ClassRoom
 function makeCeiling(x, y, z, pos_x, pos_y, pos_z) {
     var cubeGeometry = new THREE.BoxGeometry(x, y, z);
     var texture = new THREE.TextureLoader().load('ClassRoom_Ceiling.jpg');
     var cubeMaterial = new THREE.MeshBasicMaterial({ map: texture });
     var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-        
-    //add shadow render
-    //cube.castShadow = true;
     cube.receiveShadow = true;
 
     cube.position.x = pos_x;
@@ -116,51 +126,57 @@ function makeCeiling(x, y, z, pos_x, pos_y, pos_z) {
     cube.position.z = pos_z;
     ClassRoom.add(cube);
 }
-//강의실 렌더링
+//This function for drawing ClassRoom space
+//This function is called in Main.js
 function drawClassRoom(B208D) {
     for (var i = 0; i < 16; i++) {
         for (var j = 0; j < 9; j++) {
-            if (!(i == 15 && j == 4) && !(i == 15 && j == 5)){
-               makeGround(50.6, 0.6, 50.6, 2.5 + 50 * i, 0.5, 0 + 50 * j);
+            if (!(i == 15 && j == 4) && !(i == 15 && j == 5)) {
+                makeGround(50.6, 0.6, 50.6, 2.5 + 50 * i, 0.5, 0 + 50 * j);
             }
-            else{
+            else {
                 makeGround(50.6, 0.6, 50.6, 2.5 + 50 * i, -15.5, 0 + 50 * j);
             }
             makeCeiling(50.6, 0.6, 50.6, 2.5 + 50 * i, -15.5, 0 + 50 * j);
         }
     }
+    //Making for entrance
     makeWall3(20.6, 0.6, 100.6, 2.5 + 750, -10.5, 0 + 200);
     makeWall4(50.6, 0.6, 20.6, 2.5 + 775, -10.5, 0 + 250);
     makeWall4(50.6, 0.6, 20.6, 2.5 + 775, -10.5, 0 + 150);
 
-    //양 옆벽
+    //Side wall
     for (var i = 0; i < 16; i++) {
         for (var j = 0; j < 2; j++) {
             makeWall1(50.6, 0.6, 100.6, 2.5 + 50 * i, 35 + 100 * j, -25);
-            if(i!=15)
+            if (i != 15)
                 makeWall1(50.6, 0.6, 100.6, 2.5 + 50 * i, 35 + 100 * j, 425);
         }
     }
-    makeWall2(100.6, 0.6, 50.6, 753.5, 35 + 100 , -25 + 400);
-    makeWall2(100.6, 0.6, 50.6, 753.5, 35 + 0 , -25 + 400);
+    makeWall2(100.6, 0.6, 50.6, 753.5, 35 + 100, -25 + 400);
+    makeWall2(100.6, 0.6, 50.6, 753.5, 35 + 0, -25 + 400);
 
     makeWall1(50.6, 0.6, 100.6, 2.5 + 50 * 15, 35 + 100 * 0, 375);
     makeWall1(50.6, 0.6, 100.6, 2.5 + 50 * 15, 35 + 100 * 1, 375);
-
+    //Inside wall
     for (var i = 0; i < 9; i++) {
         for (var j = 0; j < 2; j++) {
             makeWall2(100.6, 0.6, 50.6, 2.5, 35 + 100 * j, -25 + 50 * i);
-            if (!(i == 4 && j == 0) && !(i == 5 && j == 0)&&!(i == 4 && j == 1) && !(i == 5 && j == 1))
+            if (!(i == 4 && j == 0) && !(i == 5 && j == 0) && !(i == 4 && j == 1) && !(i == 5 && j == 1))
                 makeWall2(100.6, 0.6, 50.6, 802.5, 35 + 100 * j, -25 + 50 * i);
-            if((i == 4 && j == 1) || (i == 5 && j == 1))
-            makeWall2(50.6, 0.6, 50.6, 802.5, 60 + 100 * j, -25 + 50 * i);
+            if ((i == 4 && j == 1) || (i == 5 && j == 1))
+                makeWall2(50.6, 0.6, 50.6, 802.5, 60 + 100 * j, -25 + 50 * i);
         }
     }
     B208D.add(ClassRoom);
 }
 
-//여기서부터는 통로 제작
-function makeWall7(x, y, z, pos_x, pos_y, pos_z,Image) {
+//------------------------
+//This is Aisle part
+//-------------------------
+
+//This function is making for wall
+function makeWall7(x, y, z, pos_x, pos_y, pos_z, Image) {
     var cubeGeometry = new THREE.BoxGeometry(x, y, z);
     var texture = new THREE.TextureLoader().load(Image);
     var cubeMaterial = new THREE.MeshBasicMaterial({ map: texture });
@@ -171,6 +187,7 @@ function makeWall7(x, y, z, pos_x, pos_y, pos_z,Image) {
     cube.position.z = pos_z + 25;
     Aisle.add(cube);
 }
+//This function is making for wall
 function makeWall8(x, y, z, pos_x, pos_y, pos_z, Image) {
     var cubeGeometry = new THREE.BoxGeometry(x, y, z);
     var texture = new THREE.TextureLoader().load(Image);
@@ -181,7 +198,7 @@ function makeWall8(x, y, z, pos_x, pos_y, pos_z, Image) {
     cube.position.z = pos_z + 25;
     Aisle.add(cube);
 }
-//입구 &막기
+////This function is making for wall of Entrance and Exit
 function makeWall9(x, y, z, pos_x, pos_y, pos_z, Image) {
     var cubeGeometry = new THREE.BoxGeometry(x, y, z);
     var texture = new THREE.TextureLoader().load(Image);
@@ -194,50 +211,50 @@ function makeWall9(x, y, z, pos_x, pos_y, pos_z, Image) {
     Aisle.add(cube);
 }
 
-
+//This function is drawing for Aisle 
+//This function is called in Main.js
+//This functiopn add aisle in scene
 function drawAisle(B208D) {
-    //문 위벽
-    makeWall7(50.6, 0.6, 50.6, 803.5, 10 + 75 * 2, -25 + 50 * 4,'Aisle_wall.png');
-    makeWall7(50.6, 0.6, 50.6, 803.5, 10 + 75 * 2, -25 + 50 * 5,'Aisle_wall.png');
+    //Upperside tile of door 
+    makeWall7(50.6, 0.6, 50.6, 803.5, 10 + 75 * 2, -25 + 50 * 4, 'Aisle_wall.png');
+    makeWall7(50.6, 0.6, 50.6, 803.5, 10 + 75 * 2, -25 + 50 * 5, 'Aisle_wall.png');
     for (var j = 0; j < 4; j++) {
         makeWall7(100.6, 0.6, 50.6, 803.5, -70 + 100 * 1, -25 + 50 * j, 'Aisle_wall.png');
         makeWall7(100.6, 0.6, 50.6, 803.5, -70 + 100 * 2, -25 + 50 * j, 'Aisle_wall.png');
     }
-    //옆판
-    makeWall7(200.6, 0.6, 504.6, 1050, 80 ,200,'Aisle_wall_color.png');
-    //붙는판
-    makeWall7(200.6, 0.6, 204.6, 805, 80 ,350,'Aisle_Attach.jpg');
-    //나가는쪽 붙는판
-   // makeWall7(200.6, 0.6, 104.6, 805, 80 ,150,'Aisle_Attach.jpg');
-    //하판  
+    //Side wall
+    makeWall7(200.6, 0.6, 504.6, 1050, 80, 200, 'Aisle_wall_color.png');
+    //Attach wall
+    makeWall7(200.6, 0.6, 204.6, 805, 80, 350, 'Aisle_Attach.jpg');
+    //Outside Attach wall
+    
+    //Ground 
     for (var i = 0; i < 5; i++) {
         for (var j = 0; j < 10; j++) {
             makeGround(50.6, 0.6, 50.6, 800.5 + 50 * i, -15, 0 + 50 * j);
         }
     }
-    
-    //천장
-    makeWall8(250.6, 0.6, 504.6, 928.5, 178 ,200,'Aisle_ground.jpg');
-    //입구
-    makeWall9(250.6, 0.6, 200.6, 928.5, 80 ,450,'AisleEntrance.jpg');
-    //반대편 입구
-    makeWall9(250.6, 0.6, 200.6, 928.5, 80 ,-50,'Aisle_Out.jpg');
+
+    //Ceiling
+    makeWall8(250.6, 0.6, 504.6, 928.5, 178, 200, 'Aisle_ground.jpg');
+    //Entrance
+    makeWall9(250.6, 0.6, 200.6, 928.5, 80, 450, 'AisleEntrance.jpg');
+    //Outside of Entrance
+    makeWall9(250.6, 0.6, 200.6, 928.5, 80, -50, 'Aisle_Out.jpg');
 
     B208D.add(Aisle);
 }
 var texture = new THREE.TextureLoader().load('table.jpg');
-
-// 스크린 뒷판
-function makeScreen_behind( ) {
+//------------------
+//Screen part
+//------------------
+//This function is making for behind of screen
+function makeScreen_behind() {
     var cubeGeometry = new THREE.BoxGeometry(140.6, 0.6, 170.6);
-    texture.rotation = Math.PI / 360 * 180;
-    var cubeMaterial = new THREE.MeshPhongMaterial({ color: 0x000000});
+    var cubeMaterial = new THREE.MeshPhongMaterial({ color: 0x000000 });
     var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-        
-    //add shadow render
-    cube.castShadow = true;
-    //cube.receiveShadow = true;
 
+    cube.castShadow = true;
     cube.rotation.z = Math.PI * -0.5;
     cube.position.x = 39.5;
     cube.position.y = 110.5;
@@ -245,10 +262,9 @@ function makeScreen_behind( ) {
     Screen.add(cube);
 }
 
-// 스크린 앞판
-function makeScreen_front( ) {
+//This function is making for front of screen
+function makeScreen_front() {
     var cubeGeometry = new THREE.BoxGeometry(140.6, 0.6, 160.6);
-    texture.rotation = Math.PI / 360 * 180;
     var cubeMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff });
     var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
@@ -262,16 +278,13 @@ function makeScreen_front( ) {
     cube.position.z = 70;
     Screen.add(cube);
 }
-
-//스크린 상판
+//This function is making for Top of screen
 function makeScreen_top(scene) {
     var cubeGeometry = new THREE.BoxGeometry(10.6, 10.6, 182.6);
     texture.rotation = Math.PI / 360 * 180;
-    var cubeMaterial = new THREE.MeshPhongMaterial({ color:0x333333, metalness: 0.2, roughness: 0 });
+    var cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x333333, metalness: 0.2, roughness: 0 });
     var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-    //add shadow render
-    //cube.castShadow = true;
     cube.receiveShadow = true;
 
     cube.position.x = 40.5;
@@ -279,10 +292,10 @@ function makeScreen_top(scene) {
     cube.position.z = 70;
     scene.add(cube);
 }
-//스크린 밑
-function makeScreen_Cylinder(){
+//This function is making for bottom of Cylinder
+function makeScreen_Cylinder() {
     var geometry = new THREE.CylinderBufferGeometry(2.3, 2.3, 180, 32);
-    var material = new THREE.MeshPhongMaterial({ color: 0xCCCCCC });
+    var material = new THREE.MeshBasicMaterial({ color: 0xCCCCCC });
     var cylinder = new THREE.Mesh(geometry, material);
     //add shadow render
     cylinder.castShadow = true;
@@ -290,11 +303,14 @@ function makeScreen_Cylinder(){
     cylinder.position.x = 41.05;
     cylinder.position.y = 40.5;
     cylinder.position.z = 70.5;
-    cylinder.rotateX(-Math.PI *0.5);
+    cylinder.rotateX(-Math.PI * 0.5);
     Screen.add(cylinder);
 }
-//Screen drawing
-function drawScreen(scene){
+//This function is drawing for Screen
+//This function is called in Main.js
+//This functiopn add Screen in scene
+//This function return Screen for animation
+function drawScreen(scene) {
     makeScreen_behind();
     makeScreen_front();
     makeScreen_Cylinder();
--- 강의실 나들이/CLASS/Main.js
+++ 강의실 나들이/CLASS/Main.js
@@ -1,4 +1,4 @@
-//Referance 참조
+//Making connect file for colaboration
 function include(FileDir) {
    var includejs = document.createElement("script");
    includejs.type = "text/javascript";
@@ -10,7 +10,7 @@ include("JDJ.js");
 include("JSW1.js");
 include("LSJ.js");
 include("JSW1.js");
-//위에부분은 참초하는거니까 건드리지마 ㅇㅋ?
+
 var ScreenDirection = 0;
 var screenType = 0;
 var camera;
@@ -24,15 +24,16 @@ var light2;
 var lightList = [];
 var keyboard = {};
 var player = { height: HEIGHT_VALUE, speed: 6, turnSpeed: Math.PI * 0.01 };
-
+//Resize window
 function onWindowResize() {
    camera.aspect = window.innerWidth / window.innerHeight;
    camera.updateProjectionMatrix();
 }
-
+//Drawing B208D
 function drawB208D(scene) {
    var B208D = new THREE.Group();
-
+   //------------------------------------------------------
+   //Drawing part
    drawClassRoom(B208D);
    drawAisle(B208D);
    makeWhiteboard(B208D, 170, 110, 200, 9.5);
@@ -40,7 +41,7 @@ function drawB208D(scene) {
    makeScreen_top(B208D);
    var Screen = drawScreen(B208D);
    B208D.add(Screen);
-   
+   //Fluorescent Rendering
    fluorescent_line = 4;
    for (i = 0; i < fluorescent_line; i++) {
       this.createFluorescentBase0(B208D, 0, 0, 0, intensityValue, 80 * (i + (i + 1) + (0.5 * i)), 183, 110, 10);
@@ -55,35 +56,33 @@ function drawB208D(scene) {
       B208D.add(light2.target);
       B208D.add(light2);
    }
-
+   //Rendering Project Body
    this.createProjectorBody0(B208D, 0, 0, 0, 170, 165, 100, 8);
-
+   //Desk Rendering
    desk_line = 5;
    for (i = 0; i < desk_line; i++) {
       this.two_person_set(B208D, 150 + 90 * (1.3 * i), 35, 200, 2.2);
       this.three_person_set(B208D, 150 + 90 * (1.3 * i), 35, 50, 2.2);
       this.three_person_set(B208D, 150 + 90 * (1.3 * i), 35, 350, 2.2);
    }
-
+  //----------------------------------------------------
    B208D.rotateY(Math.PI / 180 * 180);
    B208D.position.x += 900;
    B208D.position.z += 400;
 
    scene.add(B208D);
 }
-
+//Window Setting part
 window.onload = function init() {
    var scene = new THREE.Scene();
    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
    var renderer = new THREE.WebGLRenderer({ antialias: true });
-
-   //For bouncing balls;
    var step = 0;
    renderer.setClearColor(0xEEEEEE);
    renderer.setPixelRatio(window.devicePixelRatio);
    renderer.setSize(window.innerWidth, window.innerHeight);
    renderer.shadowMap.enabled = true;
-
+   //Button Setting
    document.getElementById("Button1").onclick = function () {
       ScreenDirection = 1;
    }
@@ -100,7 +99,7 @@ window.onload = function init() {
    this.drawB208D(scene);
 
    document.getElementById("threejs_scene").appendChild(renderer.domElement);
-
+   //Manuplating for keyboard
    function keyDown(event) {
       keyboard[event.keyCode] = true;
    }
@@ -157,11 +156,11 @@ window.onload = function init() {
          camera.position.z = temp_z;
       }
    }
-
+   //Animation Part
    var renderScene = new function renderScene() {
       requestAnimationFrame(renderScene);
 
-      //스크린 올리기
+      //Up side  screen
       if (ScreenDirection == 1) {
          if (Screen.position.y < 130) {
             Screen.position.y += 1;
@@ -169,7 +168,7 @@ window.onload = function init() {
             ScreenDirection = 0;
          }
       }
-      //스크린 내리기
+       //Down side screen
       else if (ScreenDirection == -1) {
          if (Screen.position.y > 0) {
             Screen.position.y -= 1;
@@ -177,19 +176,21 @@ window.onload = function init() {
             ScreenDirection = 0;
          }
       }
+      //Light on 
       if(lightOnOff == 1){
          for(i=0; i<lightList.length; i++){
             lightList[i].intensity = 0.34;
             lightList[i].intensity = 0.34;
          }
       }
+      //light off 
       else if (lightOnOff == -1){         
          for(i=0; i<lightList.length; i++){
             lightList[i].intensity = 0;
             lightList[i].intensity = 0;
          }
       }
-
+      
       if ((camera.position.x > 80 && camera.position.x < 190 && camera.position.z > 75 && camera.position.z < 135) ||
          (camera.position.x > 80 && camera.position.x < 190 && camera.position.z > 220 && camera.position.z < 420) ||
          camera.position.x > 190) {
@@ -211,47 +212,3 @@ window.onload = function init() {
 
    }
 }
-
-
-//    document.getElementById("Button2").onclick = function () {
-//       radius *= 0.9;
-//       console.log("-----------------------");
-//       console.log(phi);
-//       console.log(theta);
-//       console.log(radius);
-//       console.log("-----------------------");
-//    };
-//    document.getElementById("Button3").onclick = function () {
-//       theta += dr;
-//       console.log("-----------------------");
-//       console.log(phi);
-//       console.log(theta);
-//       console.log(radius);
-//       console.log("-----------------------");
-//    };
-//    document.getElementById("Button4").onclick = function () {
-//       theta -= dr;
-//       console.log("-----------------------");
-//       console.log(phi);
-//       console.log(theta);
-//       console.log(radius);
-//       console.log("-----------------------");
-//    };
-//    document.getElementById("Button5").onclick = function () {
-//       phi += dr;
-//       console.log("-----------------------");
-//       console.log(phi);
-//       console.log(theta);
-//       console.log(radius);
-//       console.log("-----------------------");
-//    };
-//    document.getElementById("Button6").onclick = function () {
-//       phi -= dr;
-//       console.log("-----------------------");
-//       console.log(phi);
-//       console.log(theta);
-//       console.log(radius);
-//       console.log("-----------------------");
-//    };
-//    render();
-// }
